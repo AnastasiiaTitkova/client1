e2c6d5de2c5b2168d16c6b14f2d114d86e51f652
AnastasiiaTitkova <128168129+AnastasiiaTitkova@users.noreply.github.com>
2023-03-19 13:05:45 +0400

    resul2

--- src/main/java/com/example/client1/sms/Sms.java
+++ src/main/java/com/example/client1/sms/Sms.java
@@ -1,2 +1,14 @@
-package com.example.client1.sms;public class Sms {
+package com.example.client1.sms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sms {
+    @GetMapping("/sms")
+    public String sendSms(){
+        System.out.println("sms was sent");
+        return "sms was sent";
+    }
 }
+
