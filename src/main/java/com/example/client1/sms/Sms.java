75aecf478cd281ccd1e7edfca5437ceafdb4d0cd
AnastasiiaTitkova <128168129+AnastasiiaTitkova@users.noreply.github.com>
2023-03-20 10:23:51 +0400

    result

--- src/main/java/com/example/client1/sms/Sms.java
+++ src/main/java/com/example/client1/sms/Sms.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Sms {
-    @RequestMapping(value="/sms", method= RequestMethod.POST)
+    @PostMapping("/sms")
     public String sendSms(){
         System.out.println("sms was sent");
         return "sms was sent";
